15d38d08a5ea0ccce9836173bb46f61acb01f105
ThierryRakotomanana <ThierryRktm@gmail.com>
2026-08-14 01:06:36 +0300

    refactor: update config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,2 @@
-export const GITHUB_CONFIG = {
-	apiBase: import.meta.env.VITE_GITHUB_API_BASE ?? `https://api.github.com`,
-	token: import.meta.env.VITE_GITHUB_TOKEN ?? undefined
-} as const;
+export const API_BASE_URL =
+	import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080";
