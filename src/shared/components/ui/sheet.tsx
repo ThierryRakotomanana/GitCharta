a2cefcdff82e2256b3ecbf7bd31c18805f4ac630
ThierryRakotomanana <ThierryRktm@gmail.com>
2026-08-15 16:12:05 +0300

    refactor(structure): moved shared components to a dedicated folder

--- src/shared/components/ui/sheet.tsx
+++ src/shared/components/ui/sheet.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { Dialog as SheetPrimitive } from "radix-ui";
 import { XIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/shared/components/ui/button";
 
 function Sheet({ ...props }: React.ComponentProps<typeof SheetPrimitive.Root>) {
 	return <SheetPrimitive.Root data-slot='sheet' {...props} />;
